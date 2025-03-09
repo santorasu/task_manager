8fb4240f476a7b1ecd97b11372909d7ab4220062
Md Rashedul Islam <santorasu09@gmail.com>
2025-03-10 05:21:45 +0600

    This is module 15 class 1 Task Manager App

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Task Manager"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: SvgPicture.asset('assets/images/background.svg'),
+      ),
+    );
+  }
+}
